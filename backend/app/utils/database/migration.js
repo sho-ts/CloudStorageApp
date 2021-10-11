41056583c5ae7d73e4c2822e83f084da53b56250
tagawa shouta <sho.webfe@gmail.com>
2021-10-12 06:55:07 +0900

    create tableのsqlを変更

--- backend/app/utils/database/migration.js
+++ backend/app/utils/database/migration.js
@@ -2,7 +2,10 @@ const migrate = `
   create table if not exists posts (
     id int primary key not null auto_increment ,
     uid int,
-    content longtext	
+    content longtext,
+    created_at datetime default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp,
+    add del_flg int(2) default 0;
   );
 `;
 
