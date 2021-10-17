321b8ae22073b92e10fd635357b5de83fdf0002b
tagawa shouta <sho.webfe@gmail.com>
2021-10-18 04:14:41 +0900

    PageTitleコンポーネントの作成

--- frontend/src/components/atoms/PageTitle.tsx
+++ frontend/src/components/atoms/PageTitle.tsx
@@ -1,8 +1,23 @@
-const PageTitle = () => {
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-bottom: 24px;
+`
+
+const Body = styled.h1`
+  font-weight: bold;
+  border-left: 4px solid #59f;
+  padding: 8px 16px;
+  font-size: 18px;
+`;
+
+const PageTitle: React.FC = ({ children }) => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <Wrapper>
+      <Body>
+        <span>{children}</span>
+      </Body>
+    </Wrapper>
   )
 }
 
