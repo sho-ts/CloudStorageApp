6bf0a48beecab05cf1f56a7aa0cdb47fe64da59f
tagawa shouta <sho.webfe@gmail.com>
2021-10-22 07:21:39 +0900

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // corsのdoc 👉 https://docs.nestjs.com/security/cors
   await app.listen(3000);
 }
 bootstrap();
