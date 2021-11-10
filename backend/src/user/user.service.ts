3bdf94300a69f814ab002d6c39d560ff97cec3a8
tagawa shouta <sho.webfe@gmail.com>
2021-11-11 02:45:33 +0900

    cognito周り（途中）

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import aws = require('aws-sdk');
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  cognitoIdentity: aws.CognitoIdentityServiceProvider;
+
+  constructor() {
+    this.cognitoIdentity = new aws.CognitoIdentityServiceProvider({
+      region: ''
+    })
+  }
+
+  async signup(username: string, password: string): Promise<boolean> {
+    const params = {
+      ClientId: '',
+      Password: password,
+      Username: username,
+    }
+
+    try {
+      const data = await this.cognitoIdentity.signUp(params).promise();
+      console.log(data);
+      return true;
+    } catch(e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  signin(email: string, password: string) {
+
+  }
+}
