5e683b6e97a305e13a468e618db2d8a9cf5198ac
tagawa shouta <sho.webfe@gmail.com>
2021-11-13 07:54:58 +0900

    cognitoの認証状態確認ようの関数を追加

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,3 +2,4 @@ export { default as config } from './config';
 export { default as fetcher } from './fetcher';
 export { default as cognitoSignUp } from './cognitoSignUp';
 export { default as cognitoSignIn } from './cognitoSignIn';
+export { default as getCognitoUser } from './getCognitoUser';
