29634c42b6a81f666d9d2de4e9f38486a36f4533
tagawa shouta <sho.webfe@gmail.com>
2021-11-18 02:05:41 +0900

    localstrageをgetServerSidePropsで取得できないため一旦useEffectで対応

--- frontend/src/pages/posts/[id].tsx
+++ frontend/src/pages/posts/[id].tsx
@@ -1,33 +1,38 @@
 import { Layout } from '@/components/templates';
 import { Button, Box } from '@chakra-ui/react';
 import type { NextPage, GetServerSideProps } from 'next'
+import { useRouter } from "next/router";
 import { PostType } from '@/types/PostType';
 import fetchPosts from '@/utils/fetchPosts';
 import config from '@/utils/config';
+import { useState, useEffect } from 'react';
 
-type PathParams = {
-  id: string;
-}
+const Post: NextPage = () => {
+  const router = useRouter();
+  const [post, setPost] = useState<PostType>();
 
-export const getServerSideProps: GetServerSideProps = async context => {
-  const { id } = context.params as PathParams;
-  const post = await fetchPosts<PostType>(Number(id))
-
-  return {
-    props: { post }
-  }
-}
-
-const Post: NextPage<{ post: PostType }> = ({ post }) => {
+  useEffect(() => {
+    (async () => {
+      if(router.query.id) {
+        const newPost = await fetchPosts<PostType>(Number(router.query.id));
+        newPost && setPost(newPost);
+      }
+    })();
+  }, [router.query.id])
 
   return (
     <Layout>
-      {post.description}
-      <Box mt={5}>
-        <Button as="a" href={`${config.api}/post/download/?key=${post.file_path}`}>
-          ダウンロード
-      </Button>
-      </Box>
+      {post ? (
+        <>
+          {post.description}
+          <Box mt={5}>
+            <Button as="a" href={`${config.api}/post/download/?key=${post.file_path}`}>
+              ダウンロード
+            </Button>
+          </Box>
+        </>
+      ) : <></>
+      }
     </Layout>
   )
 }
