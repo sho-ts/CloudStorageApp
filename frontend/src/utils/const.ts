c0bc1316ae459d4f7a2ed32e5bc9636bab03f89b
tagawa shouta <sho.webfe@gmail.com>
2021-11-24 06:41:59 +0900

    menuの表示条件を追加

--- frontend/src/utils/const.ts
+++ frontend/src/utils/const.ts
@@ -1,9 +1,13 @@
+export enum USER_TYPE { GUEST, MEMBER, COMMON }
+
 const CONSTANT_VARIABLES = {
   navItems: [
-    { href: '/signup', innerText: '新規登録' },
-    { href: '/signin', innerText: 'ログイン' },
-    { href: '/', innerText: 'よくある質問' },
-    { href: '/', innerText: 'お問い合わせ' },
+    { href: '/signup', innerText: '新規登録', user: USER_TYPE.GUEST },
+    { href: '/signin', innerText: 'ログイン', user: USER_TYPE.GUEST },
+    { href: '/mypage', innerText: 'マイページ', user: USER_TYPE.MEMBER, },
+    { href: '/signout', innerText: 'ログアウト', user: USER_TYPE.MEMBER },
+    { href: '/', innerText: 'よくある質問', user: USER_TYPE.COMMON },
+    { href: '/', innerText: 'お問い合わせ', user: USER_TYPE.COMMON },
   ],
   siteName: 'CloudStorageApp'
 }
