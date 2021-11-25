222e9f5c4fefc2553801e6d6077ecba4317d7d78
tagawa shouta <sho.webfe@gmail.com>
2021-11-26 00:03:36 +0900

    baseスタイル追加

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import Provider from '@/provider';
+import 'destyle.css';
+import '@/assets/css/base.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
