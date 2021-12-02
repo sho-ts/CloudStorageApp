cd2eb56e2d81fd53a95b77561c7dd8da4b8ee9da
tagawa shouta <sho.webfe@gmail.com>
2021-12-02 10:43:37 +0900

    ModalのWrapper部分を分離

--- frontend/src/components/organisms/Modal.tsx
+++ frontend/src/components/organisms/Modal.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ModalWrapper } from '@/components/organisms';
 
 type Props = {
   isOpen: boolean,
@@ -11,52 +12,16 @@ const Modal: React.FC<Props> = ({
   isOpen, onClose, title, children
 }) => {
   return (
-    <Wrapper isOpen={isOpen}>
+    <ModalWrapper isOpen={isOpen} onClose={onClose} w="70%" maxW="500px">
       <Inner>
         {children}
       </Inner>
-      <CloseWrapper onClick={onClose} />
-    </Wrapper>
+    </ModalWrapper>
   )
 }
 
-const Wrapper = styled.div.attrs((props: { isOpen?: boolean }) => ({
-  style: {
-    opacity: props.isOpen ? 1 : 0,
-    visibility: props.isOpen ? 'visible' : 'hidden',
-  }
-})) <{ isOpen?: boolean }>`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0,0,0,0.2);
-  cursor: pointer;
-  padding: 16px;
-  transition: all 0.3s;
-  z-index: 9999999999999;
-`;
-
-const CloseWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 5000000;
-`;
-
 const Inner = styled.div`
-  position: relative;
   background-color: #fff;
-  width: 70%;
-  max-width: 500px;
-  z-index: 5000100;
 `;
 
 export default Modal;
