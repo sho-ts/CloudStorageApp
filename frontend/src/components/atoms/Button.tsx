7d24a1b6884d6c01143617223dbc49d12d0b95d5
tagawa shouta <sho.webfe@gmail.com>
2021-12-06 07:57:07 +0900

    hoverアニメーションを追加

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { hover } from '@mixin';
 import { CSSProperties } from 'react';
 
 type Props = {
@@ -21,7 +22,24 @@ const Button: React.FC<Props> = ({ as, href, children, style, onClick, download,
   }
 };
 
-const ButtonStyle = css`
+type ButtonStyleProps = {
+  outline?: boolean,
+}
+
+const ButtonHoverStyle = (main: string, sub: string) => hover(css`
+  background-color: ${main};
+  border-color: ${sub};
+  color: ${sub};
+`);
+
+const outlineStyle = css`
+  border: 2px solid #105cc3;
+  color: #105cc3;
+  background-color: #fff;
+  ${ButtonHoverStyle('#105cc3', '#fff')}
+`;
+
+const ButtonStyle = (props: ButtonStyleProps) => css<ButtonStyleProps>`
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -29,35 +47,20 @@ const ButtonStyle = css`
   height: 54px;
   border-radius: 28px;
   background-color: #105cc3;
+  border: 2px solid #105cc3;
   color: #fff;
   text-align:center;
   font-weight: bold;
   padding: 0 42px;
   cursor: pointer;
+  transition: all 0.3s;
+  ${ButtonHoverStyle('#fff', '#105cc3')}
+  ${props.outline && outlineStyle}
 `;
 
-const outlineStyle = css`
-  border: 2px solid #105cc3;
-  color: #105cc3;
-  background-color: #fff;
-`;
-
-type ButtonStyleProps = {
-  outline?: boolean,
-}
-
-const Default = styled.button<ButtonStyleProps>`
-  ${ButtonStyle}
-  ${props => props.outline && outlineStyle}
-`;
-const A = styled.a<ButtonStyleProps>`
-  ${ButtonStyle}
-  ${props => props.outline && outlineStyle}
-`;
-const Div = styled.div<ButtonStyleProps>`
-  ${ButtonStyle}
-  ${props => props.outline && outlineStyle}
-`;
+const Default = styled.button<ButtonStyleProps>`${props => ButtonStyle(props)}`;
+const A = styled.a<ButtonStyleProps>`${props => ButtonStyle(props)}`;
+const Div = styled.div<ButtonStyleProps>`${props => ButtonStyle(props)}`;
 
 const Inner = styled.span`
   pointer-events: none;
