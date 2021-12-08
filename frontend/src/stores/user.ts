8796a9d2eb9d691654bc417f144935b2766a6a21
s.tagawa <s.tagawa@studioboomin.com>
2021-12-08 15:36:01 +0900

    非同期でのstore更新処理のためにcreateAsyncThunkを実装

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -1,9 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { auth } from '@/utils/aws';
+
+type User = {
+  email: string,
+  isSignIn: boolean
+}
+
+export const signIn = createAsyncThunk<User, { username: string, password: string }>(
+  'user/signin',
+  async ({ username, password }) => {
+    try {
+      const res = await auth.signin(username, password);
+
+      if (!res) throw new Error();
+
+      return {
+        isSignIn: true,
+        email: username
+      };
+    } catch (e) {
+      return {
+        isSignIn: false,
+        email: ''
+      }
+    }
+  }
+);
+
+export const signOut = createAsyncThunk<boolean>(
+  'user/signout',
+  async () => {
+    try {
+      const res = await auth.signout();
+
+      if (!res) throw new Error();
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+);
 
 const slice = createSlice({
   name: 'user',
   initialState: {
-    uid: '',
     email: '',
     isSignIn: false
   },
@@ -14,8 +55,22 @@ const slice = createSlice({
         ...action.payload
       }
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(signIn.fulfilled, (state, action) => {
+      state.email = action.payload.email;
+      state.isSignIn = action.payload.isSignIn;
+    });
+    builder.addCase(signOut.fulfilled, (state, action) => {
+      if (action.payload) {
+        state.email = '';
+        state.isSignIn = false;
+      }
+    })
   }
 });
+
+
 
 export default slice.reducer;
 export const { setSignInState } = slice.actions;
