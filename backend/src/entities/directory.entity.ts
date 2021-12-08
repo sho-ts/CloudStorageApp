ca4c5c1e3ff70f35102e4234c893c7a2ee3f96f1
tagawa shouta <sho.webfe@gmail.com>
2021-12-09 04:42:25 +0900

    カラム追加

--- backend/src/entities/directory.entity.ts
+++ backend/src/entities/directory.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
 
 @Entity()
 export class Directory {
@@ -7,4 +8,13 @@ export class Directory {
 
   @Column('text')
   uid: string;
+
+  @Column('tinyint', { width: 1, default: 0 })
+  del_flg: number;
+
+  @CreateDateColumn()
+  readonly created_at?: Date;
+
+  @UpdateDateColumn()
+  readonly updated_at?: Date;
 }
