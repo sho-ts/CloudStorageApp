cd229319d9c799dee623c2fd944e7d0f498267f9
s.tagawa <s.tagawa@studioboomin.com>
2021-12-08 14:40:34 +0900

    Containerコンポーネント追加

--- frontend/src/pages/signout.tsx
+++ frontend/src/pages/signout.tsx
@@ -2,8 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { PageTitle } from '@/components/atoms';
 import { Button } from '@/components/atoms';
-import { Layout } from '@/components/templates';
-import styled from 'styled-components';
+import { Layout, Container } from '@/components/templates';
 import { auth } from '@/utils/aws';
 import { useRouter } from 'next/router';
 
@@ -25,20 +24,15 @@ const SignOut: NextPage = () => {
       <Head>
         <title>ログアウト</title>
       </Head>
-      <Inner>
+      <Container size="sm">
         <PageTitle>ログアウト</PageTitle>
         <p style={{ marginBottom: 16 }}>ログアウトする場合はこちらのボタンを押してください</p>
         <div style={{ marginBottom: 16, textAlign: 'center' }}>
           <Button onClick={onClickSignout}>ログアウト</Button>
         </div>
-      </Inner>
+      </Container>
     </Layout>
   )
 }
-
-const Inner = styled.div`
-  max-width: 500px;
-  margin: 0 auto;
-`;
 
 export default SignOut;
