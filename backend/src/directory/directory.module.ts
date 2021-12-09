0ce040c8d9e838217541eb26e86e0aeaa8886783
tagawa shouta <sho.webfe@gmail.com>
2021-12-10 08:31:01 +0900

    ディレクトリ削除する際、紐づいているファイルも削除するように修正

--- backend/src/directory/directory.module.ts
+++ backend/src/directory/directory.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DirectoryController } from './directory.controller';
+import { Post } from './../entities/post.entity';
 import { Directory } from './../entities/directory.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DirectoryService } from './directory.service';
@@ -7,7 +8,7 @@ import { AuthService } from './../auth/auth.service'
 
 @Module({
   controllers: [DirectoryController],
-  imports: [TypeOrmModule.forFeature([Directory])],
+  imports: [TypeOrmModule.forFeature([Post, Directory])],
   providers: [DirectoryService, AuthService]
 })
-export class DirectoryModule {}
+export class DirectoryModule { }
--- backend/src/directory/directory.service.ts
+++ backend/src/directory/directory.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Directory } from './../entities/directory.entity';
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './../entities/post.entity';
+import { Repository, Connection } from 'typeorm';
+import { InjectRepository, InjectConnection } from '@nestjs/typeorm';
 
 @Injectable()
 export class DirectoryService {
   constructor(
     @InjectRepository(Directory)
-    private readonly directoryRepository: Repository<Directory>
+    private readonly directoryRepository: Repository<Directory>,
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>,
+    @InjectConnection()
+    private readonly connection: Connection,
   ) { }
 
   async create(uid: string, dirName: string) {
@@ -51,12 +56,34 @@ export class DirectoryService {
   }
 
   async delete(uid: string, id: number) {
-    return await this.directoryRepository
-      .createQueryBuilder()
-      .update(Directory)
-      .set({ del_flg: 1 })
-      .where('id = :id', { id })
-      .andWhere('uid = :uid', { uid })
-      .execute();
+    const queryRunner = this.connection.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      //queryBuilderでtransction実行する際、第二引数にqueryRunnerを渡す必要がある
+      await this.postRepository
+        .createQueryBuilder(null, queryRunner)
+        .update(Post)
+        .set({ del_flg: 1 })
+        .where('directoryId = :directoryId', { directoryId: id })
+        .andWhere('uid = :uid', { uid })
+        .execute();
+
+      await this.directoryRepository
+        .createQueryBuilder(null, queryRunner)
+        .update(Directory)
+        .set({ del_flg: 1 })
+        .where('id = :id', { id })
+        .andWhere('uid = :uid', { uid })
+        .execute();
+
+      await queryRunner.commitTransaction();
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
