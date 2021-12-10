ce0b9688a71e013d5abe84aa1ff021790ea136e1
s.tagawa <s.tagawa@studioboomin.com>
2021-12-11 08:08:51 +0900

    providerをgetLayoutへ移動 & ignoreMainLayout追加

--- frontend/src/components/templates/GuestLayout.tsx
+++ frontend/src/components/templates/GuestLayout.tsx
@@ -1,13 +1,10 @@
-import Provider from '@/provider';
 import { Layout } from '@/components/templates';
 
 const GuestLayout: React.FC = ({ children }) => {
   return (
-    <Provider>
-      <Layout isGuest={true}>
-        {children}
-      </Layout>
-    </Provider>
+    <Layout isGuest={true}>
+      {children}
+    </Layout>
   )
 }
 
--- frontend/src/utils/getLayout.tsx
+++ frontend/src/utils/getLayout.tsx
@@ -1,18 +1,38 @@
 import { UserLayout, GuestLayout } from '@/components/templates';
+import Provider from '@/provider';
+import Auth from '@/provider/AuthProvider';
 import type { ReactElement } from 'react'
 
 export const getUserLayout = (page: ReactElement) => {
   return (
-    <UserLayout>
-      {page}
-    </UserLayout>
+    <Provider>
+      <Auth>
+        <UserLayout>
+          {page}
+        </UserLayout>
+      </Auth>
+    </Provider>
   )
 };
 
+getUserLayout.ignoreMainLayout = (page: ReactElement) => {
+  return (
+    <Provider>
+      <Auth>
+        <UserLayout ignoreMainLayout={true}>
+          {page}
+        </UserLayout>
+      </Auth>
+    </Provider>
+  )
+}
+
 export const getGuestLayout = (page: ReactElement) => {
   return (
-    <GuestLayout>
-      {page}
-    </GuestLayout>
+    <Provider>
+      <GuestLayout>
+        {page}
+      </GuestLayout>
+    </Provider>
   )
 };
