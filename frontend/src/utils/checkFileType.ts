de960dd6ab97a88cc446fb6ba3f323a3714fe070
tagawa shouta <sho.webfe@gmail.com>
2021-12-12 19:06:15 +0900

    ファイルタイプを調べる関数追加

---
+++ frontend/src/utils/checkFileType.ts
@@ -0,0 +1,12 @@
+export const checkFileType = (filePath: string, fileTypes: string[]) => fileTypes.filter(type => filePath.slice(-5).includes(`.${type}`)).length > 0
+
+export const isImage = (filePath: string) => checkFileType(filePath, ['jpg', 'jpeg', 'png', 'gif']);
+
+export const isMovie = (filePath: string) => checkFileType(filePath, ['mp4', 'flv', 'mov', 'wmv', 'avi', 'mpg', 'mkv']);
+
+export const isCompressed = (filePath: string) => checkFileType(filePath, ['zip', 'rar', '7z', 'gz', 'bzip', 'bzip2', 'lzh', 'cab.']);
+
+export const isCode = (filePath: string) => checkFileType(filePath, [
+  'html', 'js', 'css', 'scss', 'php', 'rb', 'py', 'ts', 'jsx', 'tsx', 'rb', 'ejs', 'pug', 'jade',
+  'dart', 'abp', 'as', 'c', 'cpp', 'cobol', 'coffee', 'cs', 'go', 'htm', 'sql', 'rs', 'scala', 'sh', 'vbs'
+]);
