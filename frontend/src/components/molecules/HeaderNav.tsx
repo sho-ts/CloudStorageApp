b9544ead2ef252c6ab9c4023c5916b01b1596ab3
tagawa shouta <sho.webfe@gmail.com>
2021-12-15 19:43:05 +0900

    スタイル調整

--- frontend/src/components/molecules/HeaderNav.tsx
+++ frontend/src/components/molecules/HeaderNav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import { mq } from '@mixin';
+import styled, { css } from 'styled-components';
+import { mq, hover } from '@mixin';
 import { USER_TYPE, NAV_ITEMS } from '@/utils/const';
 import { ModalWrapper } from '@/components/molecules';
 import Link from 'next/link';
@@ -15,42 +15,40 @@ const HeaderNav: React.FC<Props> = ({ onClose, isOpen, isGuest }) => {
   return (
     <ModalWrapper onClose={onClose} isOpen={isOpen}>
       <Inner isOpen={isOpen}>
-        <Menu>
-          {NAV_ITEMS.map((item, key) => (
-            <React.Fragment key={key}>
-              {isGuest && item.user === USER_TYPE.GUEST && (
-                <Item>
-                  <Link href={item.href}>
-                    <a onClick={onClose}>{item.innerText}</a>
-                  </Link>
-                </Item>
-              )}
-              {isGuest || item.user === USER_TYPE.MEMBER && (
-                <Item>
-                  <Link href={item.href}>
-                    <a onClick={onClose}>{item.innerText}</a>
-                  </Link>
-                </Item>
-              )}
-              {item.user === USER_TYPE.COMMON && (
-                <Item>
-                  <Link href={item.href}>
-                    <a onClick={onClose}>{item.innerText}</a>
-                  </Link>
-                </Item>
-              )}
-            </React.Fragment>
-          ))}
-          <Item>
-            <a href="https://github.com/frontTSend/CloudStorageApp" target="_blank" rel="noreferrer">GitHub</a>
-          </Item>
-        </Menu>
+        {NAV_ITEMS.map((item, key) => (
+          <React.Fragment key={key}>
+            {isGuest && item.user === USER_TYPE.GUEST && (
+              <Item>
+                <Link href={item.href}>
+                  <a onClick={onClose}>{item.innerText}</a>
+                </Link>
+              </Item>
+            )}
+            {isGuest || item.user === USER_TYPE.MEMBER && (
+              <Item>
+                <Link href={item.href}>
+                  <a onClick={onClose}>{item.innerText}</a>
+                </Link>
+              </Item>
+            )}
+            {item.user === USER_TYPE.COMMON && (
+              <Item>
+                <Link href={item.href}>
+                  <a onClick={onClose}>{item.innerText}</a>
+                </Link>
+              </Item>
+            )}
+          </React.Fragment>
+        ))}
+        <Item>
+          <a href="https://github.com/frontTSend/CloudStorageApp" target="_blank" rel="noreferrer">GitHub</a>
+        </Item>
       </Inner>
     </ModalWrapper>
   )
 }
 
-const Inner = styled.div<{ isOpen?: boolean }>`
+const Inner = styled.ul<{ isOpen?: boolean }>`
   position: absolute;
   top: 0;
   right: 0;
@@ -59,21 +57,38 @@ const Inner = styled.div<{ isOpen?: boolean }>`
   background-color: #fff;
   transition: all 0.3s;
   transform: ${props => props.isOpen ? 'none' : 'translateX(calc(100% + 10px))'};
-  padding: 32px;
   z-index: 5010000;
   ${mq()} {
     width: 300px;
   }
 `;
 
-const Menu = styled.ul`
-
-`;
-
 const Item = styled.li`
   font-size: 18px;
-  &:not(:last-child) {
-    margin-bottom: 16px;
+  border-bottom: 1px solid #ddd;
+  a {
+    position: relative;
+    display: block;
+    padding: 24px 32px 24px 32px;
+    transition: all 0.3s;
+    ${hover(css`
+      background-color: #f2f2f2;
+      &::before {
+        right: 20px;
+      }
+    `)}
+    &::before {
+      content: '';
+      position: absolute;
+      top: 50%;
+      right: 24px;
+      transform: translateY(-50%) rotate(-45deg);
+      width: 10px;
+      height: 10px;
+      border-right: 2px solid #030303;
+      border-bottom: 2px solid #030303;
+      transition: all 0.3s;
+    }
   }
 `;
 
