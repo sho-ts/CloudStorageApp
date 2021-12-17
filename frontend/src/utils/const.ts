5a90da21d71f501e25695bdad070ab438acd2b76
tagawa shouta <sho.webfe@gmail.com>
2021-12-18 08:21:10 +0900

    DISCLOSURE_TYPE追加

--- frontend/src/utils/const.ts
+++ frontend/src/utils/const.ts
@@ -2,6 +2,8 @@ export enum USER_TYPE { GUEST, MEMBER, COMMON }
 
 export enum MESSAGE_TYPE { NOTICE, ERROR };
 
+export enum DISCLOSURE_TYPE { PUBLIC, PRIVATE };
+
 export const NAV_ITEMS = [
   { href: '/signup', innerText: '新規登録', user: USER_TYPE.GUEST },
   { href: '/signin', innerText: 'ログイン', user: USER_TYPE.GUEST },
