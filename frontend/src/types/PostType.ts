1a7288445cfad8bfb74bc14a6926adf11653b293
tagawa shouta <sho.webfe@gmail.com>
2021-12-18 08:21:00 +0900

    型更新

--- frontend/src/types/PostType.ts
+++ frontend/src/types/PostType.ts
@@ -1,10 +1,16 @@
+import { DISCLOSURE_TYPE } from '@/utils/const';
+
 export type PostType = {
   id: number,
   uid: string,
-  description: string,
+  directoryId: number | null, // nullの場合は「すべて」からのみ参照できる
+  description: string, // ファイル名
   created_at: Date,
   updated_at: Date,
   del_flg: number,
   file_size: string,
-  file_path: string,
+  file_path: string, // S3のキー
+  disclosure_range: DISCLOSURE_TYPE, // 公開範囲
+  allowed_email?: string[] | null, // 非公開にしている場合、自分以外に閲覧を許可するEメール
+  password?: string | null, // 非公開にしている場合、自分以外に閲覧を許可するためのパスワード
 }
