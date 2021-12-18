e7dd4e1d977986999c5707b4d622bca35e241c41
tagawa shouta <sho.webfe@gmail.com>
2021-12-18 19:03:51 +0900

    export

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as config } from './config';
 export { default as createPagination } from './createPagination';
 export { default as getFileExtension } from './getFileExtension';
+export { default as queryBuilder } from './queryBuilder';
