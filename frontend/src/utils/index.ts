7c5b47933d08060d1880b6680d0e49d792347f3a
tagawa shouta <sho.webfe@gmail.com>
2021-12-31 20:07:54 +0900

    オブジェクトからcognitoUserAttributeを生成するメソッド追加

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as config } from './config';
+export { default as createCognitoUserAttributes} from './createCognitoUserAttributes';
 export { default as createPagination } from './createPagination';
 export { default as getFileExtension } from './getFileExtension';
 export { default as queryBuilder } from './queryBuilder';
