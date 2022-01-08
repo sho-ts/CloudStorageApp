828408549421e7894448009289b26e33343daf2a
tagawa shouta <sho.webfe@gmail.com>
2022-01-09 05:45:52 +0900

    axiosのインスタンスを生成する関数を追加

---
+++ frontend/src/utils/createAxiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { auth } from '@/utils/aws';
+import { config } from '@/utils';
+
+const createAxiosInstance = async (isAuth = true) => {
+  if(!isAuth) return axios.create({baseURL: config.api});
+
+  const { token, user } = await auth.getIdTokenAndUser();
+
+  if (!user) throw new Error();
+
+  return axios.create({
+    baseURL: config.api,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+export default createAxiosInstance;
