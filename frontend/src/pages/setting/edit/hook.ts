6c48442db09607cb82fed01cb1ee64afcf40e7d6
tagawa shouta <sho.webfe@gmail.com>
2022-01-11 01:31:52 +0900

    ユーザー情報更新の処理を追加

---
+++ frontend/src/pages/setting/edit/hook.ts
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useSelector, useDispatch, useFlash } from '@/hooks';
+import { changeUserData } from '@/stores/user/asyncThunk';
+import { MESSAGE_TYPE } from '@/utils/const'
+
+const useLogic = () => {
+  const flash = useFlash();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+  const [name, setName] = useState('');
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setName(user.name);
+    setIsMounted(true);
+
+    return () => {
+      setIsMounted(false);
+    }
+  }, [user]);
+
+  const save = async () => {
+    if (!name) {
+      flash({
+        message: '未入力の項目があります',
+        type: MESSAGE_TYPE.ERROR
+      })
+      return;
+    }
+
+    try {
+      await dispatch(changeUserData({ name })).unwrap();
+      flash({
+        message: 'ユーザー情報を更新しました',
+        type: MESSAGE_TYPE.NOTICE
+      })
+
+      router.push('/setting');
+    } catch (e) {
+      flash({
+        message: (e as { errorMessage: string }).errorMessage,
+        type: MESSAGE_TYPE.ERROR
+      })
+    }
+  }
+
+  return {
+    isMounted, name, setName, save
+  }
+}
+
+export default useLogic;
