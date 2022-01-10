8b44ea2c6849cd24fed6b1b9fca404f1475fc1a7
tagawa shouta <sho.webfe@gmail.com>
2022-01-11 02:33:50 +0900

    ストレージ容量を保存するように変更

--- frontend/src/pages/setting/hook.ts
+++ frontend/src/pages/setting/hook.ts
@@ -1,10 +1,24 @@
 import { useMemo } from 'react';
 import { useSelector } from '@/hooks';
-import { PLAN_TYPE } from '@/utils/const';
+import { PLAN_TYPE, STORAGE_TYPE } from '@/utils/const';
+import { translateByte } from '@/utils';
 
 const useLogic = () => {
   const user = useSelector(state => state.user);
   const isGuest = useMemo(() => user.email === `__guest__${process.env.NEXT_PUBLIC_GUEST_KEY}`, [user]);
+  const storage = useMemo(() => {
+    if (isGuest) return STORAGE_TYPE.GUEST;
+
+    switch (user.plan) {
+      case 0:
+        return STORAGE_TYPE.FREE;
+      case 1:
+        return STORAGE_TYPE.PREMIUM;
+      default:
+        return STORAGE_TYPE.FREE;
+    }
+  }, [isGuest, user])
+
   const userDatas = useMemo(() => {
     const datas = [
       { heading: 'ユーザー名', value: user.name },
@@ -21,13 +35,13 @@ const useLogic = () => {
           }
         })()
       },
-      { heading: 'ストレージ', value: user.storage }
+      { heading: 'ストレージ', value: `${(Math.round(translateByte(user.storage, 'mb') * 10) / 10)}MB / ${storage}MB` }
     ]
 
     isGuest || datas.splice(0, 0, { heading: 'メールアドレス', value: user.email },)
 
     return datas
-  }, [user, isGuest])
+  }, [user, isGuest, storage])
 
   return {
     userDatas,
